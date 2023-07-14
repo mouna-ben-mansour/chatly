b1441a5efcb6fbf4956aba6c08a4145d7d970752
mouna-ben-mansour <benmansour.mouna2@gmail.com>
2023-07-15 00:49:52 +0100

    user search and user conversation

--- src/components/Chats.jsx
+++ src/components/Chats.jsx
@@ -1,29 +1,44 @@
-import React from "react";
+import React, { useState , useContext, useEffect } from "react";
+import { doc, onSnapshot  } from "firebase/firestore";
+import { db } from "../firebase";
+import { AuthContext } from "../context/AuthContext";
 import User from "../images/user2.jpg"
 const Chats = () => {
+    const [chats, setChats] = useState([]);
+    const [err,setErr] = useState(false);
+    const { currentUser } = useContext(AuthContext);
+
+    useEffect (() => {
+       const getChats = () => {
+            try {
+                const unsub = onSnapshot(doc(db, "userChats", currentUser.uid), (doc) => {
+                    console.log("Current data: ", doc.data());
+                    setChats(doc.data());
+                });
+                // clean data
+                return ( () => {unsub()})
+                
+            } catch (error) {
+                setErr(true);
+            }
+       }
+       currentUser.uid && getChats();
+    },[currentUser.uid])
+
+
     return ( 
         <div className="chats">
-            <div className="user-chat">
-                <img src={ User } alt=""/>
-                <div className="user-info">
-                    <span>Abdallah</span>
-                    <p>Hello!</p>
-                </div>
-            </div>
-            <div className="user-chat">
-                <img src={ User } alt=""/>
-                <div className="user-info">
-                    <span>Abdallah</span>
-                    <p>Hello!</p>
-                </div>
-            </div>
-            <div className="user-chat">
-                <img src={ User } alt=""/>
-                <div className="user-info">
-                    <span>Abdallah</span>
-                    <p>Hello!</p>
-                </div>
-            </div>
+            {
+                Object.entries(chats)?.map((chat) => (
+                    <div className="user-chat" key={chat[0]}>
+                        <img src={ chat[1].userInfo.photoURL } alt=""/>
+                        <div className="user-info">
+                            <span>{chat[1].userInfo.displayName}</span>
+                            <p>{chat[1].userInfo.lastMessage?.text}</p>
+                        </div>
+                    </div>
+                ))
+            }
         </div>
      );
 }
--- src/components/Search.jsx
+++ src/components/Search.jsx
@@ -1,18 +1,81 @@
-import React from "react";
-import User from "../images/user.jpg"
+import React, { useState , useContext } from "react";
+import { doc, collection, query, where, getDocs, getDoc, setDoc, updateDoc, serverTimestamp } from "firebase/firestore";
+import { db } from "../firebase";
+import { AuthContext } from "../context/AuthContext";
 const Search = () => {
+    const [username, setUsername] = useState('');
+    const [user, setUser] = useState(null);
+    const [err,setErr] = useState(false);
+    const { currentUser } = useContext(AuthContext);
+    const handleSearch = async() => {
+        const usersRef = collection(db, "users");
+        const q = query(usersRef, where("displayName", "==", username));
+        try {
+            const querySnapshot = await getDocs(q);
+            querySnapshot.forEach((doc) => {
+            // doc.data() is never undefined for query doc snapshots
+            setUser(doc.data());
+            });
+            
+        } catch (error) {
+            setErr(true);
+        }
+
+    }
+    const handleKey = (e) => {
+        e.code === "Enter" && handleSearch();
+    }
+
+    // when select user 
+    const handleSelect = async () => {
+        // check whether the group ( chats in firestore ) exists, if not just create new one
+
+        const combinedId = currentUser.uid > user.uid ? currentUser.uid + user.uid : user.uid + currentUser.uid ;
+        //create user chats
+        try {
+            const res = await getDoc(doc(db, "chats", combinedId));
+            if(!res.exists()) {
+                await setDoc(doc(db, "chats", combinedId), { messages: []});
+                await updateDoc (doc(db, "userChats", currentUser.uid),{
+                    [combinedId + ".userInfo"]: {
+                        uid:user.uid,
+                        displayName: user.displayName,
+                        photoURL: user.photoURL
+                    },
+                    [combinedId + ".date"]: serverTimestamp()
+
+                })
+                await updateDoc (doc(db, "userChats", user.uid),{
+                    [combinedId + ".userInfo"]: {
+                        uid:currentUser.uid,
+                        displayName: currentUser.displayName,
+                        photoURL: currentUser.photoURL
+                    },
+                    [combinedId + ".date"]: serverTimestamp()
+
+                })
+            } 
+        } catch (err) {
+            // setErr(true);
+            console.log(err)
+        }
+        setUser(null);
+        setUsername('');
+
+    }
     return ( 
         <div className="search">
             <div className="search-form">
-                <input type="text" placeholder="Find a user"/>
+                <input type="text" placeholder="Find a user" onKeyDown={handleKey} onChange={e=>setUsername(e.target.value)} value={username}/>
             </div>
-            <div className="user-chat">
-                <img src={ User } alt=""/>
+            {err && <span>{err}</span>}
+            {user && <div className="user-chat" onClick={handleSelect}>
+                <img src={ user.photoURL } alt=""/>
                 <div className="user-info">
-                    <span>Sarra</span>
+                    <span>{user.displayName}</span>
                     <p>Hello!</p>
                 </div>
-            </div>
+            </div>}
         </div>
      );
 }
