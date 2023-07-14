4b4360fa948d0fac4ef7c2be8b6a3e96c7ce5b16
mouna ben mansour <benmansour.mouna2@gmail.com>
2023-07-14 12:34:44 +0200

    chat content

--- src/components/Message.jsx
+++ src/components/Message.jsx
@@ -1,7 +1,17 @@
 import React from "react";
+import User from "../images/user.jpg"
 const Message = () => {
     return ( 
-        <div className="message">message</div>
+        <div className="message owner">
+            <div className="message-info">
+                <img src={ User } alt=""/>
+                <span>just now</span>
+            </div>
+            <div className="message-content">
+                <p>Good morning!</p>
+                <img src={ User } alt=""/>
+            </div>
+        </div>
      );
 }
  
