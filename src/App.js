ceb91db383c3406ad59a2492365a20ec669af184
mouna ben mansour <benmansour.mouna2@gmail.com>
2023-07-14 15:18:41 +0200

    react router

--- src/App.js
+++ src/App.js
@@ -1,9 +1,31 @@
 import Home from "./pages/Home";
 import Register from "./pages/Register";
+import Login from "./pages/Login";
 import './style.scss';
+import { createRoot } from "react-dom/client";
+import {
+  createBrowserRouter,
+  RouterProvider,
+  Route,
+  Link,
+} from "react-router-dom";
 function App() {
+  const router = createBrowserRouter([
+    {
+      path: "/",
+      element: <Home/>,
+    },
+    {
+      path: "register",
+      element: <Register/>,
+    },
+    {
+      path: "login",
+      element: <Login/>,
+    },
+  ]);
   return (
-    <Register/>
+    <RouterProvider router={router} />
   );
 }
 
--- src/pages/Register.jsx
+++ src/pages/Register.jsx
@@ -4,9 +4,11 @@ import { createUserWithEmailAndPassword, updateProfile } from "firebase/auth";
 import { auth, storage, db} from "../firebase";
 import { ref, uploadBytesResumable, getDownloadURL } from "firebase/storage";
 import { doc, setDoc } from "firebase/firestore"; 
+import { useNavigate } from "react-router-dom";
 
 const Register = () => {
     const [error, setError] = useState(false);
+    const navigate = useNavigate()
     const handleSubmit = async (e) => {
         e.preventDefault();
         
@@ -35,6 +37,7 @@ const Register = () => {
                         });
                          // Add a new document in collection "userChats"
                          await setDoc(doc(db, "userChats", res.user.uid), {});
+                         navigate('/');
                     } catch (error) {
                         console.log(error)
                         setError(true);
